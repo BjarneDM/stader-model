44759d891524d4ee1ea0bd4b333c181db620bbb6
BjarneDM <bjarne@mathiesen.info>
2022-02-17 20:58:47 +0100

    On branch DataObjectsConstruct Changes to be committed: 	modified:   php/src/Model/Tables/PlaceOwner/PlaceOwners.php

--- php/src/Model/Tables/PlaceOwner/PlaceOwners.php
+++ php/src/Model/Tables/PlaceOwner/PlaceOwners.php
@@ -1,24 +1,13 @@
 <?php namespace Stader\Model\Tables\PlaceOwner ;
 
 use \Stader\Model\Abstract\DataObjectsDao ;
+use \Stader\Model\Traits\DataObjectsConstruct ;
 
 class PlaceOwners extends DataObjectsDao
 {
-    public static $allowedKeys = [] ;
-    public static $thisClass   = '' ;
+    private static $baseClass = '\\Stader\\Model\\Tables\\PlaceOwner\\PlaceOwner' ;
 
-    function __construct ( ...$args )
-    {   // echo "class UGroups extends DataObjectsDao __construct" . \PHP_EOL ;
-        // var_dump( $args ) ;
-
-        self::$allowedKeys = ( new \ArrayObject( PlaceOwner::$allowedKeys ) )->getArrayCopy() ;
-        $this->keysAllowed = ( new \ArrayObject( PlaceOwner::$allowedKeys ) )->getArrayCopy() ;
-        self::$thisClass = PlaceOwner::$thisClass ;
-        $this->class     = PlaceOwner::$thisClass ;
-        parent::__construct() ;
-        $this->setupData( $args ) ;
-
-    }
+    use DataObjectsConstruct ;
 
 }
 
