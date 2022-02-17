efe4d39664b862d6517bb004433a22f4c9a4445d
BjarneDM <bjarne@mathiesen.info>
2022-02-17 22:03:58 +0100

    On branch DataObjectsConstruct Changes to be committed: 	modified:   php/src/Model/Tables/Beredskab/BeredskabLogs.php 	modified:   php/src/Model/Tables/Beredskab/Beredskabs.php

--- php/src/Model/Tables/Beredskab/BeredskabLogs.php
+++ php/src/Model/Tables/Beredskab/BeredskabLogs.php
@@ -1,6 +1,7 @@
 <?php namespace Stader\Model\Tables\Beredskab ;
 
 use \Stader\Model\Abstract\LogObjectsDao ;
+use \Stader\Model\Traits\DataObjectsConstruct ;
 
 /*
 
@@ -18,21 +19,9 @@ create table if not exists beredskab_log
 
 class BeredskabLogs extends LogObjectsDao
 {
-    public static $allowedKeys = [] ;
-    public static $thisClass   = '' ;
+    private static $baseClass = '\\Stader\\Model\\Tables\\Beredskab\\BeredskabLog' ;
 
-    function __construct ( ...$args )
-    {   // echo "class UGroups extends DataObjectsDao __construct" . \PHP_EOL ;
-        // var_dump( $args ) ;
-
-        self::$allowedKeys = ( new \ArrayObject( BeredskabLog::$allowedKeys ) )->getArrayCopy() ;
-        $this->keysAllowed = ( new \ArrayObject( BeredskabLog::$allowedKeys ) )->getArrayCopy() ;
-        self::$thisClass = BeredskabLog::$thisClass ;
-        $this->class     = BeredskabLog::$thisClass ;
-        parent::__construct() ;
-        $this->setupData( $args ) ;
-
-    }
+    use DataObjectsConstruct ;
 
 }
 
--- php/src/Model/Tables/Beredskab/Beredskabs.php
+++ php/src/Model/Tables/Beredskab/Beredskabs.php
@@ -1,24 +1,13 @@
 <?php namespace Stader\Model\Tables\Beredskab ;
 
 use \Stader\Model\Abstract\DataObjectsDao ;
+use \Stader\Model\Traits\DataObjectsConstruct ;
 
 class Beredskabs extends DataObjectsDao
 {
-    public static $allowedKeys = [] ;
-    public static $thisClass   = '' ;
+    private static $baseClass = '\\Stader\\Model\\Tables\\Beredskab\\Beredskab' ;
 
-    function __construct ( ...$args )
-    {   // echo "class UGroups extends DataObjectsDao __construct" . \PHP_EOL ;
-        // var_dump( $args ) ;
-
-        self::$allowedKeys = ( new \ArrayObject( Beredskab::$allowedKeys ) )->getArrayCopy() ;
-        $this->keysAllowed = ( new \ArrayObject( Beredskab::$allowedKeys ) )->getArrayCopy() ;
-        self::$thisClass = Beredskab::$thisClass ;
-        $this->class     = Beredskab::$thisClass ;
-        parent::__construct() ;
-        $this->setupData( $args ) ;
-
-    }
+    use DataObjectsConstruct ;
 
 }
 
