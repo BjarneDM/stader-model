4c1da2b6c71203f1a930679955f9d1755789a71d
BjarneDM <bjarne@mathiesen.info>
2022-02-17 20:19:58 +0100

    On branch DataObjectsConstruct Changes to be committed: 	modified:   php/src/Model/Tables/Group/UGroups.php

--- php/src/Model/Tables/Group/UGroups.php
+++ php/src/Model/Tables/Group/UGroups.php
@@ -1,24 +1,13 @@
 <?php namespace Stader\Model\Tables\Group ;
 
 use \Stader\Model\Abstract\DataObjectsDao ;
+use \Stader\Model\Traits\DataObjectsConstruct ;
 
 class UGroups extends DataObjectsDao
 {
-    public static $allowedKeys = [] ;
-    public static $thisClass   = '' ;
+    private static $baseClass = '\\Stader\\Model\\Tables\\Group\\UGroup' ;
 
-    function __construct ( ...$args )
-    {   // echo "class UGroups extends DataObjectsDao __construct" . \PHP_EOL ;
-        // var_dump( $args ) ;
-
-        self::$allowedKeys = ( new \ArrayObject( UGroup::$allowedKeys ) )->getArrayCopy() ;
-        $this->keysAllowed = ( new \ArrayObject( UGroup::$allowedKeys ) )->getArrayCopy() ;
-        self::$thisClass = UGroup::$thisClass ;
-        $this->class     = UGroup::$thisClass ;
-        parent::__construct() ;
-        $this->setupData( $args ) ;
-
-    }
+    use DataObjectsConstruct ;
 
 }
 
