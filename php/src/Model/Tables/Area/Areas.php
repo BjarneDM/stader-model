dd50d77efb229718fe0da67c428e3b35aa1c173e
BjarneDM <bjarne@mathiesen.info>
2022-02-17 21:00:40 +0100

    On branch DataObjectsConstruct Changes to be committed: 	modified:   php/src/Model/Tables/Area/Areas.php

--- php/src/Model/Tables/Area/Areas.php
+++ php/src/Model/Tables/Area/Areas.php
@@ -1,24 +1,13 @@
 <?php namespace Stader\Model\Tables\Area ;
 
 use \Stader\Model\Abstract\DataObjectsDao ;
+use \Stader\Model\Traits\DataObjectsConstruct ;
 
 class Areas extends DataObjectsDao
 {
-    public static $allowedKeys = [] ;
-    public static $thisClass   = '' ;
+    private static $baseClass = '\\Stader\\Model\\Tables\\Area\\Area' ;
 
-    function __construct ( ...$args )
-    {   // echo "class UGroups extends DataObjectsDao __construct" . \PHP_EOL ;
-        // var_dump( $args ) ;
-
-        self::$allowedKeys = ( new \ArrayObject( Area::$allowedKeys ) )->getArrayCopy() ;
-        $this->keysAllowed = ( new \ArrayObject( Area::$allowedKeys ) )->getArrayCopy() ;
-        self::$thisClass = Area::$thisClass ;
-        $this->class     = Area::$thisClass ;
-        parent::__construct() ;
-        $this->setupData( $args ) ;
-
-    }
+    use DataObjectsConstruct ;
 
 }
 
